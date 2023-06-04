ba25284fef4e00635db8b76eb1e0e12b0f0f547f
akira saito <akirassan@hotmail.com>
2023-06-03 23:43:06 -0300

    atualizacao

--- Trabalhos exame/Trabalho1/dados_plottly.py
+++ Trabalhos exame/Trabalho1/dados_plottly.py
@@ -14,13 +14,28 @@
 import plotly.express as px
 import pandas as pd
 
-dados = pd.read_excel('dados\Dados_plotly (1).xlsx')
+dados = pd.read_excel('Trabalho1\dados\Dados_plotly (1).xlsx')
 
 df = pd.DataFrame(dados)
-dados_x = df['mês']
-dados_y = df['Tipo']
 
 # %%
+#    - valor x mês por tipo, gráfico de linha.
+
+dados_x = df['mês']
+dados_y = df['Tipo']
 fig = px.line(x = dados_x, y = dados_y)
 fig.show()
 # %%
+#    - histograma numero funcionários x  cidades.
+
+dados_x = df['cidade']
+dados_y = df['n_funcionario']
+fig = px.histogram(x= dados_x, y= dados_y)
+fig.show()
+# %%
+#    - histograma valor x cidades em cada tipo.
+
+dados_x = df[['valor']]
+dados_y = df[['cidade']]
+fig = px.histogram(x= dados_x, y= dados_y)
+fig.show()
